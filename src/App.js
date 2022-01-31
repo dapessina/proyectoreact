67b34b02631c4c9c4d4bb3cd9f540a78c43838cd
dapessina <87141250+dapessina@users.noreply.github.com>
2022-01-31 13:37:01 -0600

    Add files via upload
    carga de app

---
+++ src/App.js
@@ -0,0 +1,25 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Dana 
+        </p>
+        <a>
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrßer"
+          Learn Reactr
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
